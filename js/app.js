a8435bd81a23be5c4081987073511b7ba5629c1c
seiwookkim <macbookair9@MacBook-Air-2.local>
2016-02-28 11:05:37 -0500

    Adding Starter page content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 $(document).foundation();
+
+$(window).on('load', function(){
+		
+				
+				$('.gallery-nav li').each(function(){
+					var src = $(this).find('img').attr('src');
+					$(this).css('background-image', 'url("'+src+'")');
+					$(this).find('img').remove();
+					$(this).addClass('gallery-cell');
+				})
+				
+				$('.main-gallery').css('height', $('.main-gallery').width())
+		
+				var main_gallery = new Flickity( '.main-gallery', {
+					prevNextButtons: false,
+					pageDots: false, 
+					imagesLoaded: true,
+					setGallerySize: false
+				} );
+		
+				var gallery_nav = new Flickity( '.gallery-nav', {
+					asNavFor:'.main-gallery',
+					prevNextButtons: false,
+					pageDots: false,
+					contain: true,
+					imagesLoaded: true
+				} );
+		
+				
+				
+				
+	
+		})
